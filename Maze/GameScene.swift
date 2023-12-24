7ede13210d148c7145baf2f6fdec941cbd50a750
Manalllllllll <98577200+Manalllllllll@users.noreply.github.com>
2023-12-24 17:14:35 +0300

    hii

--- Maze/GameScene.swift
+++ Maze/GameScene.swift
@@ -10,62 +10,83 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var thePlayer:SKSpriteNode = SKSpriteNode()
+    var chompAction:SKAction = SKAction(named:"Chomp")!
+    var theCamera:SKCameraNode = SKCameraNode()
+    var arrayOfAvailableSpots=[CGPoint]()
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        for node in self.children{
+            if(node is SKTileMapNode){
+                if let theMap:SKTileMapNode=node as? SKTileMapNode{
+                    setUpSceneWithMap( map: theMap)
+                }
+            }
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        if(self.childNode(withName: "Player") != nil ){
+            thePlayer = self.childNode(withName: "Player") as! SKSpriteNode
+            thePlayer.run(chompAction, withKey: "Chomp")
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            
         }
+        if (self.childNode(withName: "Camera") != nil){
+            theCamera=self.childNode(withName: "Camera") as! SKCameraNode
+        }
+        self.camera = theCamera
+    }
+    
+    func setUpSceneWithMap(map: SKTileMapNode){
+        
+        let tileMap=map
+        let tileSize=tileMap.tileSize
+        let halfWidth = CGFloat(tileMap.numberOfColumns)/2.0 * tileSize.width
+        let halfHeight = CGFloat(tileMap.numberOfRows)/2.0 * tileSize.height
+        for col in 0..<tileMap.numberOfColumns {
+            for row in 0..<tileMap.numberOfRows{
+                if let _ = tileMap.tileDefinition(atColumn: col, row: row){
+                    //a painted tile exists
+                }else{
+                    // a painted tile does NOT exists here, so our charcter could move to it
+                    let x = CGFloat(col) * tileSize.width - halfWidth + (tileSize.width/2)
+                    let y = CGFloat(row) * tileSize.height - halfHeight + (tileSize.height/2)
+                    print("no tile at \(x), \(y)")
+                    let newPoint: CGPoint = CGPoint(x:x, y:y)
+                    let newPointConverted:CGPoint = self.convert(newPoint, from: tileMap)
+                    arrayOfAvailableSpots.append(newPointConverted)
+                    let newNode:SKSpriteNode = SKSpriteNode (imageNamed:"Rectangle")
+                   newNode.position = newPointConverted
+                    self.addChild(newNode)
+                    newNode.name = "Rectangle"
+                    
+                }
+            }
+        }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        theCamera.position = thePlayer.position
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+     
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+     
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+      
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
+      
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -82,7 +103,5 @@ class GameScene: SKScene {
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+    
 }
---
+++ butterflyGIF.swift
@@ -0,0 +1,8 @@
+//
+//  butterflyGIF.swift
+//  Maze
+//
+//  Created by silva on 08/06/1445 AH.
+//
+
+import Foundation
